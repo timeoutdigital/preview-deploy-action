a1b0b9b092de2b6e86ecc58d0152d1da97b612ef
Jonathan Burman <jonathan.burman@timeout.com>
2020-11-05 10:56:11 +0000

    add prettier, respond to feedback

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 async function action() {
-    const core = require("@actions/core")
-    const exec = require("@actions/exec")
-    const github = require("@actions/github")
+    const core = require("@actions/core");
+    const exec = require("@actions/exec");
+    const github = require("@actions/github");
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
 
     const appName = core.getInput(`appName`);
     if (!appName) {
         console.log("App Name is required");
-        return; 
+        return;
     }
 
     const action = core.getInput(`action`);
     if (!action) {
         console.log("Action is required");
-        return; 
+        return;
     }
-    
+
     const domainName = core.getInput(`domainName`);
     const imageTag = core.getInput(`imageTag`);
     const graffitiSecret = core.getInput(`graffitiSecret`);
     const accountId = core.getInput(`accountId`);
 
-    if (action === 'install') {
-        
+    if (action === "install") {
         if (!domainName) {
             console.log("domainName is required");
-            return; 
+            return;
         }
 
         if (!imageTag) {
             console.log("imageTag is required");
-            return; 
+            return;
         }
-        
+
         if (!graffitiSecret) {
             console.log("graffitiSecret is required");
-            return; 
+            return;
         }
 
         if (!accountId) {
             console.log("accountId is required");
-            return; 
+            return;
         }
     }
-    
-    // hostname that will resolve to deployment 
+
+    // hostname that will resolve to deployment
     const hostname = `www-${github.context.issue.number}.${domainName}`;
-    const namespace = `pr-${github.context.issue.number}`;   
+    const namespace = `pr-${github.context.issue.number}`;
     // Location of helm chart
     const chart_path = `charts/${appName}`;
 
-    let cmdOutput = '';
-    let cmdError = '';
+    let cmdOutput = "";
+    let cmdError = "";
 
     const options = {};
     options.listeners = {
@@ -60,21 +59,19 @@ async function action() {
         },
         stderr: (data) => {
             cmdError += data.toString();
-        }
+        },
     };
-    
-    if (action === 'install') {
-        
-        try {
 
+    if (action === "install") {
+        try {
             const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
             const helm_args = [
-                'upgrade', 
+                "upgrade",
                 appName,
-                '--install', 
-                '--wait', 
-                '--create-namespace', 
-                '-n', 
+                "--install",
+                "--wait",
+                "--create-namespace",
+                "-n",
                 namespace,
                 `${GITHUB_WORKSPACE}/${chart_path}`,
                 "-f",
@@ -88,14 +85,13 @@ async function action() {
                 "--set",
                 `image.tag=${imageTag}`,
                 "--set",
-                `image.repository=${accountId}.dkr.ecr.eu-west-1.amazonaws.com/gp-web`
-            ] 
+                `image.repository=${accountId}.dkr.ecr.eu-west-1.amazonaws.com/gp-web`,
+            ];
 
-            await exec.exec('helm', helm_args, options);
-            console.log(`Install  Deployment `)
-           
-        } catch(err) {
-            console.log(`output ${cmdOutput}`)
+            await exec.exec("helm", helm_args, options);
+            console.log(`Install  Deployment `);
+        } catch (err) {
+            console.log(`output ${cmdOutput}`);
             core.setOutput("output", cmdError);
             await octokit.issues.createComment({
                 ...github.context.repo,
@@ -104,45 +100,51 @@ async function action() {
             });
             return;
         }
-        
+
         core.setOutput("output", cmdOutput);
         await octokit.issues.createComment({
             ...github.context.repo,
             issue_number: github.context.issue.number,
             body: `Success: http://${hostname}`,
         });
-
-    } else if (action === 'delete') {
+    } else if (action === "delete") {
         try {
-            await exec.exec('helm', ['status', appName, '-n', namespace], options);
-            console.log(`Deployment exists`)
+            await exec.exec(
+                "helm",
+                ["status", appName, "-n", namespace],
+                options
+            );
+            console.log(`Deployment exists`);
             try {
-                await exec.exec('helm', ['delete', appName, '-n', namespace], options);
-                console.log(`Delete Deployment `)
-            } catch(err) {
-                console.log(`output ${cmdError}`)
+                await exec.exec(
+                    "helm",
+                    ["delete", appName, "-n", namespace],
+                    options
+                );
+                console.log(`Delete Deployment `);
+            } catch (err) {
+                console.log(`output ${cmdError}`);
                 core.setOutput("output", cmdError);
-                console.log(`Failed to delete deployment`)
-                return
+                console.log(`Failed to delete deployment`);
+                return;
             }
-        } catch(err) {
-            console.log(`output ${cmdError}`)
+        } catch (err) {
+            console.log(`output ${cmdError}`);
             core.setOutput("output", cmdError);
-            console.log(`No stack present`)
-            return 
+            console.log(`No stack present`);
+            return;
         }
         core.setOutput("output", cmdOutput);
-        return 
-        
+        return;
     } else {
-        console.log(`${action} not implemented`)
+        console.log(`${action} not implemented`);
         core.setOutput("output", `${action} not implemented`);
-        return
+        return;
     }
 }
 
-if (typeof require !== 'undefined' &&  require.main === module) {
+if (typeof require !== "undefined" && require.main === module) {
     action();
 }
 
-module.exports = action
+module.exports = action;
